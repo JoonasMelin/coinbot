4234d984daa118de0079c4168e27e45314aeb427
Joonas <joonas.melin@gmail.com>
2018-05-27 16:21:39 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='coinbot',
+    version='',
+    packages=['coinbot', 'coinbot.utilities'],
+    url='',
+    license='',
+    author='Joonas Melin',
+    author_email='',
+    description=''
+)
